2dfbc68df1cacc8a9b646f69a4a071fcf30f9502
PoliticalSnail <zioxan52@gmail.com>
2025-08-26 22:54:25 -0500

    frontend

---
+++ Frontend/fork-electron/main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  mainWindow.loadURL('http://localhost:5199'); // Must be running already!
+  mainWindow.webContents.openDevTools(); // Optional, but useful
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
