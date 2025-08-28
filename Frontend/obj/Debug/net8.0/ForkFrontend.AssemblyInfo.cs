1a02f36677251ce2b0b5cf09ee81f440d458e0ef
PoliticalSnail <zioxan52@gmail.com>
2025-08-28 14:43:44 -0500

    fork

--- Frontend/obj/Debug/net8.0/ForkFrontend.AssemblyInfo.cs
+++ Frontend/obj/Debug/net8.0/ForkFrontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ForkFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2853e803803ff95b1d7aae5ff60b0e6b61d44bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd6d1a63a1d356dbe06db0b016dcc188eb9da34d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ForkFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ForkFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
